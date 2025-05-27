d69b578dbdf07e8cf66c9c10ae2b11e74862c426
emtech-user <emtech-user@PC-number>
2025-05-27 14:42:30 +0900

    Weight　高杉

--- MediMal/src/main/java/model/entity/Weight.java
+++ MediMal/src/main/java/model/entity/Weight.java
@@ -1,7 +1,43 @@
 package model.entity;
 
+import java.util.Date;
+
 public class Weight {
 	
+	
+	private Date inputDate;
+	private int measureWeight;
+	private String unit;
+	
+	public Weight() {
+		
+	}
+
+	
+	//getter setter
+	public Date getInputDate() {
+		return inputDate;
+	}
+
+	public void setInputDate(Date inputDate) {
+		this.inputDate = inputDate;
+	}
+
+	public int getMeasureWeight() {
+		return measureWeight;
+	}
+
+	public void setMeasureWeight(int measureWeight) {
+		this.measureWeight = measureWeight;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
 	
 	
 	
