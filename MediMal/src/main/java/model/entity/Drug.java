312a71e673724bce7b1ad414c49198c9e2a5da5a
emtech-user <emtech-user@PC-number>
2025-05-27 14:56:09 +0900

    Drug田中

--- MediMal/src/main/java/model/entity/Drug.java
+++ MediMal/src/main/java/model/entity/Drug.java
@@ -1,5 +1,42 @@
 package model.entity;
 
-public class Drug {
+import java.sql.Date;
 
+public class Drug {
+	
+	
+	private Date date;
+	private int measureWeight;
+	private String unit;
+	
+	public Drug() {
+		
+	}
+
+	public Date getDate() {
+		return date;
+	}
+	
+	
+	//getter setter
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public int getMeasureWeight() {
+		return measureWeight;
+	}
+
+	public void setMeasureWeight(int measureWeight) {
+		this.measureWeight = measureWeight;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	
 }
