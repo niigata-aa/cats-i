7bdc8d83a0b35598cd64bf34fd5d5085dc387420
emtech-user <emtech-user@PC-number>
2025-05-27 15:47:17 +0900

    EmployeeDAO田中

--- MediMal/src/main/java/model/dao/EmployeeDAO.java
+++ MediMal/src/main/java/model/dao/EmployeeDAO.java
@@ -1,10 +1,33 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entity.EmployeeBean;
+
 public class EmployeeDAO {
 	
-	public boolean LoginCheck(String empID,String Password) {
+	public boolean loginCheck(String empID,String Password) {
 		
 		return ;
 	}
-
+	
+	public List<EmployeeBean> selectAllEmp(){
+		
+	}
+	
+	public List<EmployeeBean> selectImpByField(EmployeeBean Employee){
+		
+		return ;
+	}
+	
+	public void insertEmp(EmployeeBean Employee) {
+		
+	}
+	
+	public int updateEmp(EmployeeBean Employee) {
+		
+		return ;
+	}
+	
+	public int hiddenEmp()
 }
