5054255880dfe9c132b79349010443dffc0601e1
emtech-user <emtech-user@PC-number>
2025-05-27 14:32:46 +0900

    ConnectionManager田中

---
+++ MediMal/src/main/java/model/dao/ConnectionManagerMedical.java
@@ -0,0 +1,20 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManagerMedical {
+	/*接続設定の定義*/
+	private static final String URL = "jdbc:mysql://localhost:3306/データベース名";
+	private static final String USER = "ユーザ名";
+	private static final String PASSWORD = "パスワード";
+	
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
+
+}
