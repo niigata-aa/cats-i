13a0d95b9dc86172597c48c8fc01e3ea0756b2a3
emtech-user <emtech-user@PC-number>
2025-05-27 14:55:12 +0900

    Feed 高杉

--- MediMal/src/main/java/model/entity/Feed.java
+++ MediMal/src/main/java/model/entity/Feed.java
@@ -1,5 +1,51 @@
 package model.entity;
 
-public class Feed {
+import java.util.Date;
 
+public class Feed {
+	
+	private Date date;
+	private String content;
+	private int amount;
+	private String unit;
+	
+	public Feed() {
+		
+	}
+
+	//getter setter
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	
+	
+	
 }
