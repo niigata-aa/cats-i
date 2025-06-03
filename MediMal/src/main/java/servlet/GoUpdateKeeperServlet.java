fe2659f5e983f5b631bfd851ee8e7019eee2c4c0
emtech-user <emtech-user@PC-number>
2025-06-03 09:18:36 +0900

    tanaka

--- MediMal/src/main/java/servlet/GoUpdateKeeperServlet.java
+++ MediMal/src/main/java/servlet/GoUpdateKeeperServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,20 +11,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.entity.EmployeeBean;
+
 /**
  * Servlet implementation class GoUpdateKeeperServlet
  */
 @WebServlet("/goUpdateKeeper")
 public class GoUpdateKeeperServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GoUpdateKeeperServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GoUpdateKeeperServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -39,34 +42,64 @@ public class GoUpdateKeeperServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		request.setCharacterEncoding("UTF-8");
-		
+
 		//ログインしてるか
 		String url;
 
 		if (session.getAttribute("LoginID")!=null) {
-			url = "menu.jsp";
+			url = "updateKeeper.jsp";
 		}else {
 			url ="login.jsp";
 		}
-		
-		
-		
+
+		EmployeeBean updateEmp = new EmployeeBean();
+
 		String 	empID 		= request.getParameter("empID");
-		String 	lastName 	= request.getParameter("lastName");
-		String 	firstName 	= request.getParameter("firstName");
-		String 	getArea 	= request.getParameter("area");
-		
-		int 	workingNow	=Integer.parseInt(request.getParameter("workingNow"));
-		
-		request.setAttribute("empID",empID);
-		request.setAttribute("lastName",lastName);
-		request.setAttribute("firstName",firstName);
-		request.setAttribute("getArea",getArea);
-		request.setAttribute("workingNow", workingNow);
-		
-		// リクエストの転送
-		RequestDispatcher rd = request.getRequestDispatcher("updateKeeper.jsp");
-		rd.forward(request, response);
+		//		String 	lastName 	= request.getParameter("lastName");
+		//		String 	firstName 	= request.getParameter("firstName");
+		//		String 	getArea 	= request.getParameter("area");
+		//		
+		//		int 	workingNow	=Integer.parseInt(request.getParameter("workingNow"));
+		//		
+		//		request.setAttribute("empID",empID);
+		//		request.setAttribute("lastName",lastName);
+		//		request.setAttribute("firstName",firstName);
+		//		request.setAttribute("getArea",getArea);
+		//		request.setAttribute("workingNow", workingNow);
+
+		for(EmployeeBean TmpEmployee : (List<EmployeeBean>) session.getAttribute("employeeList")){
+			if(TmpEmployee.getEmpID().equals(empID)) {
+
+				empID= (String) session.getAttribute("empID");
+				String 	lastName 	= (String) session.getAttribute("lastName");
+				String 	firstName 	= (String) session.getAttribute("firstName");
+				int 	areaID 	= (int) session.getAttribute("areaID");
+				String  gender  = (String) session.getAttribute("gender");
+				int		workingNow  = (int) session.getAttribute("workingNow");
+				String  photoURL	= (String) session.getAttribute("photoURL");
+
+				updateEmp.setEmpID(empID);
+
+				updateEmp.setLastName(lastName);
+
+				updateEmp.setFirstName(firstName);
+
+				updateEmp.setAreaID(areaID);
+
+				updateEmp.setGender(gender);
+
+				System.out.println(workingNow);
+				updateEmp.setWorkingNow(workingNow);
+				
+				updateEmp.setPhotoURL(photoURL);
+
+				session.setAttribute("workingNow", workingNow);
+
+				session.setAttribute("updateEmployee", updateEmp);
+			}
+			// リクエストの転送
+			RequestDispatcher rd = request.getRequestDispatcher(url);
+			rd.forward(request, response);
+		}
 	}
 }
-	
